958d47df6f8fccf3a7fc95fa7338e64274763345
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-03-24 07:57:58 +0530

    add

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const path = require("path");
 const authMiddleware = require("./src/middleware/authMiddleware")
 
 const app=express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
