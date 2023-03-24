488eab9aaad5954a6721f0415aba04458ffac642
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2023-03-24 11:37:59 +0530

    done

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-const express = require ("express")
-const Connect = require("./src/config/db")
-require("dotenv").config()
-const registerRouter=require("./src/routes/userRouter")
-const loginRouter=require("./src/routes/userRouter")
-const cors=require("cors")
+const express = require("express");
+const Connect = require("./src/config/db");
+require("dotenv").config();
+const registerRouter = require("./src/routes/registerRouter");
+const loginRouter = require("./src/routes/loginRouter");
+const cors = require("cors");
 const path = require("path");
-const authMiddleware = require("./src/middleware/authMiddleware")
+const authMiddleware = require("./src/middleware/authMiddleware");
 
-const app=express()
-const port = process.env.PORT || 3000
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Serve the frontend build files
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+// Routes
+app.use("/user", registerRouter);
+app.use("/user", loginRouter);
+app.get("/", authMiddleware, (req, res) =>
+  res.status(200).json({ message: "Hello" })
+);
+
+// Catch-all route to serve the index.html file
 app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
-    function (err) {
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"), function (
+    err
+  ) {
+    if (err) {
       res.status(500).send(err);
     }
-  );
+  });
 });
 
-app.get("/" ,authMiddleware ,(req , res)=> res.status(200).json({message:"Hello"}))
-
-app.use("/user" , registerRouter)
-app.use("/user" , loginRouter)
-
-
-app.listen(port ,async()=>{
+app.listen(port, async () => {
   try {
-    await Connect()
+    await Connect();
     console.log(`Server running on localhost:${port}`);
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-})
+});
